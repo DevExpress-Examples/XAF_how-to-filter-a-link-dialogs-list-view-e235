8a59f0a30accde7dbb51a731b100d6a04f2a741e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToFilterListViewInLinkDialog.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace HowToFilterListViewInLinkDialog.Module {
+   public sealed partial class HowToFilterListViewInLinkDialogModule : ModuleBase {
+      public HowToFilterListViewInLinkDialogModule() {
+         InitializeComponent();
+      }
+   }
+}
